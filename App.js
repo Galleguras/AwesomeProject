6b729b679cecd98857de3cbf4c032b9c81ce0bad
Pablo Gallego Carmona <pablogallegocarmona@gmail.com>
2022-09-08 18:49:00 +0200

    instalando-y-configurando-react-navigation

--- App.js
+++ App.js
@@ -2,15 +2,19 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import LoginForm from "./src/components/LoginForm";
 import Saludar from "./src/components/Saludar";
+import "react-native-gesture-handler";
+import { NavigationContainer } from "@react-navigation/native";
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Curso React Native</Text>
-      <Saludar firstName="Pablo" lastName="Perez Sol" />
-      <Saludar firstName="Luis" lastName="Lopez Gla" />
-      <Saludar firstName="Sergio" lastName="Nistal Ver" />
-      <Saludar />
-    </View>
+    <NavigationContainer>
+      <View style={styles.container}>
+        <Text>Curso React Native</Text>
+        <Saludar firstName="Pablo" lastName="Perez Sol" />
+        <Saludar firstName="Luis" lastName="Lopez Gla" />
+        <Saludar firstName="Sergio" lastName="Nistal Ver" />
+        <Saludar />
+      </View>
+    </NavigationContainer>
   );
 }
 
